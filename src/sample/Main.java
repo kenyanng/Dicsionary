57d5f6d04df4bfa0a7117d9246ae5ec701cec9c8
kenyanng <65432870+kenyanng@users.noreply.github.com>
2020-10-18 17:54:08 +0700

    Update Main.java

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,11 +1,33 @@
 package sample;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 import java.io.IOException;
-import java.util.List;
-public class Main {
-    public static void main (String[] args) throws IOException {
-        DictionaryCommandline moi = new DictionaryCommandline();
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        primaryStage.setTitle("Dictionary");
+        Scene scene = new Scene(root);
+        scene.getStylesheets().add(getClass().getResource("appllication.css").toExternalForm());
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) throws IOException {
+        launch(args);
+        /*DictionaryCommandline moi = new DictionaryCommandline();
         moi.dictionaryBasic();
         moi.dictionaryAdvanced();
         moi.showAllWord();
+        
+         */
+         
     }
 }
+
