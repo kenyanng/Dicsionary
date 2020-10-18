9601896092d25028f6c782c0e881d3083f3fbc0b
kenyanng <65432870+kenyanng@users.noreply.github.com>
2020-10-18 17:47:35 +0700

    fix

--- src/sample/DictionaryCommandline.java
+++ src/sample/DictionaryCommandline.java
@@ -1,37 +1,35 @@
 package sample;
-import java.awt.*;
 import java.io.IOException;
-import java.util.List;
-import java.util.Scanner;
-import java.util.ArrayList;
 
 public class DictionaryCommandline extends Dictionary{
-    public void showAllWord() {
-        System.out.println("No\t\t|English\t\t|Vietnamese");
-        for (int i = 0; i < dic.size() ; i++) {
-            System.out.println((i + 1) + "\t\t|" +   dic.get(i).getword());
-        }
-    }
+	public void showAllWord() {
+		System.out.println("No\t\t|English\t\t|Vietnamese");
+		for (int i = 0; i < dic.size() ; i++) {
+			System.out.println((i + 1) + "\t\t|" +   dic.get(i).getword());
+		}
+	}
 
-    public void dictionaryBasic() throws IOException {
-        DictionaryManagement moi = new DictionaryManagement();
-        moi.insertFromCommandline();
-      //  showAllWord();
-    }
-    public void  dictionaryAdvanced() throws IOException  {
-        DictionaryManagement moi = new DictionaryManagement();
-        moi.insertFromFile();
-        String s = "Hi";
-       // moi.remove(s);
-       // moi.edit(s);
-      //  moi.add();
-      //  moi.edit(s);
-        //showAllWord();
-        moi.dictionaryExportToFile();
-    }
-    public  void  dictionarySearcher() throws IOException {
-        DictionaryManagement moi = new DictionaryManagement();
-        String s = "Hi";
-        moi.dictionaryLookup(s);
-    }
+	public void dictionaryBasic() throws IOException {
+		DictionaryManagement moi = new DictionaryManagement();
+		moi.insertFromCommandline();
+		//  showAllWord();
+	}
+	public void  dictionaryAdvanced() throws IOException  {
+		DictionaryManagement moi = new DictionaryManagement();
+		moi.insertFromFile();
+		/*String s = "Hi";
+		moi.remove(s);
+		moi.edit(s);
+		moi.add();
+		moi.edit(s);
+		showAllWord();
+
+		 */
+		moi.dictionaryExportToFile();
+	}
+	public  void  dictionarySearcher() throws IOException {
+		DictionaryManagement moi = new DictionaryManagement();
+		String s = "Hi";
+		moi.dictionaryLookup(s);
+	}
 }
